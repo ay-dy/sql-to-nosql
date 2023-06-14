8b39403845d0077661849fbb0861cfca95ed7147
Adrian <aydy@pm.me>
2023-06-14 09:10:03 +0100

    Connection info component implemented.

---
+++ src/components/global/ConnectionInfo.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import DatabaseIcon from '../../assets/images/connection-info/database.png';
+import UserIcon from '../../assets/images/connection-info/user.png';
+import ComponentContainer from './ComponentContainer';
+
+const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  background-color: #25546d;
+  outline: 2px solid #2f6a8a;
+  border-radius: 12px;
+  padding: 10px 12px;
+`;
+
+const Info = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 8px 0;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 16px;
+`;
+
+const TextWrapper = styled.span`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 500;
+  font-size: 16px;
+`;
+
+export default function ConnectionInfo({ user, database }) {
+  return (
+    <ComponentContainer alignItems={'flex-start'} maxWidth={'384px'}>
+      <InfoContainer>
+        <Info>
+          <Icon src={UserIcon} />
+          <TextWrapper>{user}</TextWrapper>
+        </Info>
+        <Info>
+          <Icon src={DatabaseIcon} />
+          <TextWrapper>{database}</TextWrapper>
+        </Info>
+      </InfoContainer>
+    </ComponentContainer>
+  );
+}
