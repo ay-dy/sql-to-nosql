0f6fbcab865f34a2bb49e44136ab8885333fee8f
Adrian <aydy@pm.me>
2023-06-14 09:14:17 +0100

    MongoDB form functionality in progress.

--- src/components/main/MongoDBConnectionForm.jsx
+++ src/components/main/MongoDBConnectionForm.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormBase from '../form/FormBase';
 import FormInputField from '../form/FormInputField';
 import FormLabel from '../form/FormLabel';
 import FormSubmitButton from '../form/FormSubmitButton';
+// import Toast from '../global/Toast';
 
 export default function MongoDBConnectionForm() {
+  const [uriValidity, setUriValidity] = useState(true);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const newUriValidity = !!e.target.value;
+  }
+
   return (
-    <FormBase>
+    <FormBase onSubmit={(e) => submitHandler(e)}>
       <FormLabel text={'MongoDB Connection'} />
       {/* <Toast /> */}
-      <FormInputField name={'URI'} />
-      <FormSubmitButton />
+      <FormInputField
+        name={'URI'}
+        isValid={uriValidity}
+        onBlur={(e) => setUriValidity(!!e.target.value)}
+      />
+      <FormSubmitButton text={'Connect'} variant={'green'} />
     </FormBase>
   );
 }
