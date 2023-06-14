cedacd8211cf8527f8c986437a0a13f0ee53cc40
Adrian <aydy@pm.me>
2023-06-14 09:09:09 +0100

    Added functionality and styling to the button.

--- src/components/global/Button.jsx
+++ src/components/global/Button.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const colors = {
+  green: { light: '#118054', dark: '#0e6b46' },
+  red: { light: '#B72B2B', dark: '#9a2424' }
+}
+
 const StyledButton = styled.button`
   width: 100%;
   padding: ${({ fontSize }) => fontSize ? `${fontSize * 0.75}px` : "12px"};
@@ -11,17 +16,25 @@ const StyledButton = styled.button`
   font-weight: 500;
   line-height: inherit; 
   color: inherit;
-  background-color: #118054;
-  transition: background-color 0.125s linear;
+  background-color: ${({ disabled, variant }) => disabled ? colors[variant].dark : colors[variant].light};
+  transition: background-color ${({ disabled }) => disabled ? '0s' : '0.1s'} linear;
 
   &:hover {
-    cursor: pointer;
-    background-color: #0e6b46;
+    cursor: ${({ disabled }) => disabled ? 'not-allowed' : 'pointer'};
+    background-color: ${({ variant }) => colors[variant].dark};
   }
 `;
 
-export default function Button({ text, fontSize }) {
+export default function Button({ text, name, fontSize, variant, disabled }) {
   return (
-    <StyledButton fontSize={fontSize}>{text}</StyledButton>
+    <StyledButton
+      type={'submit'}
+      name={name}
+      fontSize={fontSize}
+      variant={variant}
+      disabled={disabled}
+    >
+      {text}
+    </StyledButton>
   );
 }
