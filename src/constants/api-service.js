4fe90eaeb5205c9b98a8d4cfdd1fab9bc580dbc3
Adrian <aydy@pm.me>
2023-06-14 09:13:47 +0100

    Added a file with API connection functions.

---
+++ src/constants/api-service.js
@@ -0,0 +1,42 @@
+const url = 'http://localhost:3000/api';
+
+export async function connectSQL(config) {
+  let status, message;
+
+  try {
+    const res = await fetch(url + '/sql/connect', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(config)
+    });
+    const data = await res.json();
+
+    status = res.status;
+    message = data.message;
+
+  } catch {
+    status = 4;
+    message = 'API at rest. Who\'s the alarm?';
+  }
+
+  return { status, message };
+}
+
+export async function disconnectSQL() {
+  let status, message;
+
+  try {
+    const res = await fetch(url + '/sql/disconnect', {
+      method: 'POST'
+    });
+    const data = await res.json();
+
+    status = res.status;
+    message = data.message;
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  return { status, message };
+}
