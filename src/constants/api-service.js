ad9662f38c6abef2412d933ecfec92bdb658edf5
Adrian <aydy@pm.me>
2023-06-20 07:46:07 +0100

    MongoDB connection functions implemented.

--- src/constants/api-service.js
+++ src/constants/api-service.js
@@ -35,7 +35,49 @@ export async function disconnectSQL() {
     message = data.message;
 
   } catch (error) {
-    console.log(error);
+    status = 4;
+    message = 'API at rest. Who\'s the alarm?';
+  }
+
+  return { status, message };
+}
+
+export async function connectMongoDB(config) {
+  let status, message;
+
+  try {
+    const res = await fetch(url + '/mongodb/connect', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(config)
+    });
+    const data = await res.json();
+
+    status = res.status;
+    message = data.message;
+  } catch (error) {
+    status = 4;
+    message = 'API at rest. Who\'s the alarm?';
+  }
+
+  return { status, message };
+}
+
+export async function disconnectMongoDB() {
+  let status, message;
+
+  try {
+    const res = await fetch(url + '/mongodb/disconnect', {
+      method: 'POST'
+    });
+    const data = await res.json();
+
+    status = res.status;
+    message = data.message;
+
+  } catch (error) {
+    status = 4;
+    message = 'API at rest. Who\'s the alarm?';
   }
 
   return { status, message };
