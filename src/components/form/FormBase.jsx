6e270af116ca99a795a55c02b9aaadd12d77870f
Adrian <aydy@pm.me>
2023-07-19 07:58:43 +0100

    Made background optional for convert component

--- src/components/form/FormBase.jsx
+++ src/components/form/FormBase.jsx
@@ -9,13 +9,15 @@ const Container = styled.form`
   justify-content: center;
   margin-bottom: 32px;
   padding: 12px 24px;
-  border: 2px solid #214a60;
+  border: ${({ hideBg }) => hideBg ? 'none' : '2px solid #214a60'};
   border-radius: 16px;
-  background-color: #19394a;
+  background-color: ${({ hideBg }) => hideBg ? 'transparent' : '#19394a'};
 `;
 
-export default function FormBase({ children, onSubmit }) {
+export default function FormBase({ children, onSubmit, hideBg }) {
   return (
-    <Container onSubmit={onSubmit}>{children}</Container>
+    <Container onSubmit={onSubmit} hideBg={hideBg}>
+      {children}
+    </Container>
   );
 }
